ab658be4b0ff3c27defb13bf2477d086e654d48f
Reillyhewitson <reilly.hewitson@utcreading.org.uk>
2019-01-31 13:37:11 +0000

    Finish app, add screenshots

--- lib/home.dart
+++ lib/home.dart
@@ -60,12 +60,141 @@ class PlantItem extends StatefulWidget{
   State<StatefulWidget> createState() => PlantItemState();
 }
 
+enum WaterState{
+  watered,
+  not
+}
+
 class PlantItemState extends State<PlantItem>{
+  @override
+  void initState(){
+    super.initState();
+    
+    this.setWater(global.status[widget.index]);
+  }
+
+  WaterState _waterState = WaterState.not;
+
+  setWater(status){
+    if(status == false){
+      _waterState = WaterState.not;
+    }
+    else{
+      _waterState = WaterState.watered;
+    }
+    
+
+  }
+  
+
+  void water(){
+    global.status[widget.index] = true;
+    setState(() {
+      _waterState = WaterState.watered;
+    });
+
+  }
+
+  void not(){
+    global.status[widget.index] = false;
+    setState(() {
+          _waterState = WaterState.not;
+        });
+  }
   
   Widget build(BuildContext context){
-    return ListTile(
+    return InkWell( 
+      onTap: (){
+        Navigator.push(
+            context, 
+            MaterialPageRoute(
+              builder: (BuildContext context) => PlantDetail(plant: widget.plant, status: global.status[widget.index], index: widget.index,)
+            )
+          );
+      },
+      child: ListTile(
       leading: Image.network(widget.plant["img"], height: 50.0, width: 50.0,),
       title: Text(widget.plant["name"]),
-      trailing: global.status[widget.index] == true ? FlatButton(child: Text("Watered", style: TextStyle(color: Colors.green)), onPressed: () => global.status[widget.index] = true) : FlatButton(child: Text("Not Watered", style: TextStyle(color: Colors.red)), onPressed: () => global.status[widget.index] = true,));
+      trailing: _waterState == WaterState.watered ? FlatButton(child: Text("Watered", style: TextStyle(color: Colors.green)), onPressed: () => not()) : FlatButton(child: Text("Not Watered", style: TextStyle(color: Colors.red)), onPressed: () => water(),)
+      )
+    );
   }
 }
+
+class PlantDetail extends StatefulWidget{
+  PlantDetail({this.plant, this.status, this.index});
+  final Map plant;
+  final bool status;
+  final int index;
+  
+  @override
+  State<StatefulWidget> createState() => PlantDetailState();
+}
+
+class PlantDetailState extends State<PlantDetail>{
+  @override
+  void initState(){
+    super.initState();
+    
+    this.setWater(global.status[widget.index]);
+  }
+
+  WaterState _waterState = WaterState.not;
+
+  setWater(status){
+    if(status == false){
+      _waterState = WaterState.not;
+    }
+    else{
+      _waterState = WaterState.watered;
+    }
+    
+
+  }
+
+  void water(){
+    global.status[widget.index] = true;
+    setState(() {
+      _waterState = WaterState.watered;
+    });
+
+  }
+
+  void not(){
+    global.status[widget.index] = false;
+    setState(() {
+          _waterState = WaterState.not;
+        });
+  }
+
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.plant["name"]), automaticallyImplyLeading: true, ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[ 
+                  Container(child: Image.network(widget.plant["img"])),
+            Container( 
+              padding: EdgeInsets.fromLTRB(16.0, 16.0, 0, 0), 
+              child:ListTile( 
+                title: Text(widget.plant["name"], style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold)),
+                trailing: _waterState == WaterState.watered ? FlatButton(child: Text("Watered", style: TextStyle(color: Colors.green)), onPressed: () => not()) : FlatButton(child: Text("Not Watered", style: TextStyle(color: Colors.red)), onPressed: () => water(),) ,
+              )
+            ),
+            Container(
+              child: Column(
+                children: <Widget>[
+                  Text("Description", style: TextStyle(fontSize: 20.0)),
+                  Text(widget.plant["desc"], textAlign: TextAlign.center,),
+                  Divider(),
+                  Text("How to care for", style: TextStyle(fontSize: 20.0)),
+                  Text(widget.plant["howTo"], textAlign: TextAlign.center,)
+                ]
+              )
+            )
+          ]
+        )
+      );
+  }
+}
+      
